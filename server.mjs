d7e9fb428a1e4b2ec62555f21db6ac6d891a214b
KashanAdnan <khanzaidaboy@gmail.com>
2023-12-22 13:19:09 +0500

    some-code

--- server.mjs
+++ server.mjs
@@ -21,6 +21,6 @@ cloudinary.config({
 });
 connectDataBase();
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on Port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log(`listening on Port ${3000}`);
 });
